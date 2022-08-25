3aec9e877315752e74fdde4f74a48773af06bbf4
Thiago Novaes <77932135+Thiago-NovaesB@users.noreply.github.com>
2022-08-24 23:31:49 -0300

    test

--- problems/problem9.py
+++ problems/problem9.py
@@ -5,6 +5,7 @@ Created on Fri Aug 12 21:42:42 2022
 @author: thiag
 """
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find the product abc.
+
 def brute_force():
     for a in range(1,500):
         for b in range(1,500):
